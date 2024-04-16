ac6613abc445499d2f7748c3df9d3bd67b0f6604
adampoewilson <adam62385@gmail.com>
2024-04-16 14:57:28 -0400

    Initial Commit

---
+++ Rock Paper Scissors challenge/Rock_Paper_Scissors_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_Scissors_challengeApp.swift
+//  Rock Paper Scissors challenge
+//
+//  Created by Adam Wilson on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_Scissors_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
