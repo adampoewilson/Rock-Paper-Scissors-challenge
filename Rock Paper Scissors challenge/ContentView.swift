9e5302692ce560256e578fc9e4430753178bec43
adampoewilson <adam62385@gmail.com>
2024-04-19 13:21:32 -0400

    starting game logic for Rock Ppaer Scissors challenge

--- Rock Paper Scissors challenge/ContentView.swift
+++ Rock Paper Scissors challenge/ContentView.swift
@@ -8,16 +8,104 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+//  What cpu & user can select
+    
+    @State private var RPS = ["Rock", "Paper", "Scissors"]
+    
+//    placeholder alerts for buttons
+    
+    @State private var alertRock = false
+    
+    @State private var alertPaper = false
+    
+    @State private var alertScissors = false
+    
+//  Win or lose state
+    
+    @State private var WorL = ["Win", "Lose"]
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        NavigationView {
+            
+            ZStack {
+                
+                Color.gray
+                
+                VStack {
+                    
+                    // Rock button
+                    
+                    Button("Rock") {
+                        
+                        alertRock = true
+                        
+                    }
+                    .background(.black)
+                    .foregroundColor(.cyan)
+                    .font(.title)
+                    .alert("You picked Rock.", isPresented: $alertRock) {
+                        
+                        Button("OK", role: .cancel) { }
+                        
+                    }
+                    
+                    Spacer().frame(height: 15)
+                   
+                    Button("Paper") {
+                        
+                        alertPaper = true
+                        
+                    }
+                    .background(.black)
+                    .foregroundColor(.cyan)
+                    .font(.title)
+                    .alert("You picked Paper.", isPresented: $alertPaper) {
+                        
+                        Button("Ok", role: .cancel) {   }
+                        
+                       
+                    }
+                    
+                    Spacer().frame(height: 15)
+                    
+                    Button("Scissors") {
+                        
+                        alertScissors = true
+                        
+                    }
+                    .background(.black)
+                    .foregroundColor(.cyan)
+                    .font(.title)
+                    .alert("You picked scissors.", isPresented: $alertScissors) {
+                        
+                        Button("OK", role: .cancel) {   }
+                        
+                    }
+                }
+                .padding()
+                
+                
+                
+              
+                    
+                }
+            }
+            
         }
-        .padding()
+        
     }
+    
+// game logic
+
+func cpu_choice() {
+    
+  
+    
 }
+
 
 #Preview {
     ContentView()
